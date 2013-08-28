1bfcf3263f1ed70a6707b658066c1efbdc2c8522
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2013-08-28 16:28:39 +0200

    SDK-11 Added runnable jar project

---
+++ openmrs-sdk-tool/src/main/java/org/openmrs/sdk/Tool.java
@@ -0,0 +1,13 @@
+package org.openmrs.sdk;
+
+/**
+ * Hello world!
+ *
+ */
+public class Tool 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
