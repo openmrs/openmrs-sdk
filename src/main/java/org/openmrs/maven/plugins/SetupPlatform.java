53fc73a56a72b137f4af0a0d20312538176f6208
diospark <dmytro.trifonov@gmail.com>
2015-06-04 02:12:31 +0300

    SDK-35 updated properties for setup-platform goal added PropertyManager class for managing properties

--- src/main/java/org/openmrs/maven/plugins/SetupPlatform.java
+++ src/main/java/org/openmrs/maven/plugins/SetupPlatform.java
@@ -5,6 +5,10 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.BuildPluginManager;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.components.interactivity.Prompter;
+import org.codehaus.plexus.components.interactivity.PrompterException;
+
+import java.util.Properties;
 
 import static org.twdata.maven.mojoexecutor.MojoExecutor.*;
 
@@ -40,6 +44,55 @@ public class SetupPlatform extends AbstractMojo {
     private String interactiveMode;
 
     /**
+     * Server id (folder name)
+     *
+     * @parameter expression="${serverId}"
+     */
+    private String serverId;
+
+    /**
+     * Platform version
+     *
+     * @parameter expression="${version} default-value="1.11.2"
+     */
+    private String version;
+
+    /**
+     * DB Driver type
+     *
+     * @parameter expression="${dbDriver}
+     */
+    private String dbDriver;
+
+    /**
+     * DB Uri
+     *
+     * @parameter expression="${dbUri}
+     */
+    private String dbUri;
+
+    /**
+     * DB User
+     *
+     * @parameter expression="${dbUser}
+     */
+    private String dbUser;
+
+    /**
+     * DB Pass
+     *
+     * @parameter expression="${dbPassword}
+     */
+    private String dbPassword;
+
+    /**
+     * Component for user prompt
+     *
+     * @component
+     */
+    private Prompter prompter;
+
+    /**
      * The Maven BuildPluginManager component.
      *
      * @component
@@ -48,13 +101,21 @@ public class SetupPlatform extends AbstractMojo {
     private BuildPluginManager pluginManager;
 
     public void execute() throws MojoExecutionException {
+        // check if user not set serverId parameter
+        if (null == serverId) try {
+            // prompt this param
+            serverId = prompter.prompt("Please specify server id:");
+        } catch (PrompterException e) {
+            e.printStackTrace();
+        }
+        //getLog().info(System.getProperty("user.home"));
         executeMojo(
-                plugin(groupId("org.openmrs.maven.archetypes"), artifactId("maven-archetype-openmrs-project"), version("1.0.2")),
+                plugin(groupId("org.openmrs.maven.archetypes"), artifactId("maven-archetype-openmrs-project"), version("1.0.0")),
                 goal("generate"),
                 configuration(
                         element(name("interactiveMode"), interactiveMode),
                         element(name("package"), "org.openmrs"),
-                        element(name("archetypeGroupId"),"org.openmrs.maven.archetypes"),
+                        element(name("archetypeGroupId"), "org.openmrs.maven.archetypes"),
                         element(name("archetypeArtifactId"), "maven-archetype-openmrs-project"),
                         element(name("archetypeVersion"), "1.0.0-SNAPSHOT")
                 ),
---
+++ src/main/java/org/openmrs/maven/plugins/utility/PropertyManager.java
@@ -0,0 +1,77 @@
+package org.openmrs.maven.plugins.utility;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Class for reading/writing .properties files
+ */
+public class PropertyManager {
+    private Properties properties;
+    private String path;
+
+    public PropertyManager() {
+        properties = new Properties();
+    }
+
+    /**
+     * Initialize
+     * @param filePath - .properties file path
+     */
+    public PropertyManager(String filePath) {
+        this();
+        path = filePath;
+        FileInputStream in = null;
+        try {
+            in = new FileInputStream(path);
+            properties.load(in);
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Get param from properties
+     * @param key - property key
+     * @return - property value
+     */
+    public String getParam(String key) {
+        return properties.getProperty(key);
+    }
+
+    /**
+     * Set param to properties object (without applying)
+     * @param key - property key
+     * @param value - value to set
+     */
+    public void setParam(String key, String value) {
+        properties.setProperty(key, value);
+    }
+
+    /**
+     * Write properties to .property file
+     */
+    public void apply() {
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(path);
+            properties.store(out, null);
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Set property and apply it
+     * @param key - property key
+     * @param value - value to set
+     */
+    public void applyParam(String key, String value) {
+        setParam(key, value);
+        apply();
+    }
+}
