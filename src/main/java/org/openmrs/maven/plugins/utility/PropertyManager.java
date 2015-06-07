1495c4781bee574c9022ce38edf28fa74f548e27
diospark <dmytro.trifonov@gmail.com>
2015-06-08 00:32:14 +0300

    SDK-35 added option if properties are not exist

--- src/main/java/org/openmrs/maven/plugins/utility/PropertyManager.java
+++ src/main/java/org/openmrs/maven/plugins/utility/PropertyManager.java
@@ -1,5 +1,6 @@
 package org.openmrs.maven.plugins.utility;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -12,9 +13,7 @@ public class PropertyManager {
     private Properties properties;
     private String path;
 
-    public PropertyManager() {
-        properties = new Properties();
-    }
+    public PropertyManager() { properties = new Properties(); }
 
     /**
      * Initialize
@@ -23,13 +22,16 @@ public class PropertyManager {
     public PropertyManager(String filePath) {
         this();
         path = filePath;
-        FileInputStream in = null;
-        try {
-            in = new FileInputStream(path);
-            properties.load(in);
-            in.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        File config = new File(path);
+        if (config.exists()) {
+            FileInputStream in = null;
+            try {
+                in = new FileInputStream(path);
+                properties.load(in);
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
