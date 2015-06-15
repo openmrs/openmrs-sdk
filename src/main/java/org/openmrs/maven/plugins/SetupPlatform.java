d91371ebcf8a15a7dafd959c1ed64f8e8692d35b
diospark <dmytro.trifonov@gmail.com>
2015-06-15 12:59:22 +0300

    SDK-35 added basedir param to execute mojo params

--- src/main/java/org/openmrs/maven/plugins/SetupPlatform.java
+++ src/main/java/org/openmrs/maven/plugins/SetupPlatform.java
@@ -1,6 +1,5 @@
 package org.openmrs.maven.plugins;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.BuildPluginManager;
@@ -13,8 +12,6 @@ import org.openmrs.maven.plugins.utility.PropertyManager;
 import org.openmrs.maven.plugins.utility.SDKConstants;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.List;
 
 import static org.twdata.maven.mojoexecutor.MojoExecutor.*;
 
@@ -146,31 +143,21 @@ public class SetupPlatform extends AbstractMojo {
                       String dbUser,
                       String dbPassword,
                       String interactiveMode) throws MojoExecutionException {
-        // path to omrs home
         File omrsPath = new File(System.getProperty("user.home"), SDKConstants.OPENMRS_SERVER_PATH);
-        // check if user not set serverId parameter
         if (serverId == null) try {
-            // default server name
             String defaultId = "server";
-            // index for search unused server name
             int indx = 0;
-            // find unused name for server
             while (new File(omrsPath, defaultId).exists()) {
                 indx++;
                 defaultId = "server" + String.valueOf(indx);
             }
-            // prompt this param
             serverId = prompter.prompt("Define value for property 'serverId': (default: '" + defaultId + "')");
-            // check if user pressed 'enter' (default value)
             if (serverId.equals("")) serverId = defaultId;
         } catch (PrompterException e) {
             e.printStackTrace();
         }
-        // path to server with serverId
         File serverPath = new File(omrsPath, serverId);
-        // check existence
         if (serverPath.exists()) throw new MojoExecutionException("Server with same id already created");
-        // execute plugin for create server
         executeMojo(
                 plugin(
                         groupId(SDKConstants.ARCH_GROUP_ID),
@@ -184,63 +171,42 @@ public class SetupPlatform extends AbstractMojo {
                         //element(name("package"), SDKConstants.PROJECT_PACKAGE),
                         element(name("archetypeGroupId"), SDKConstants.ARCH_PROJECT_GROUP_ID),
                         element(name("archetypeArtifactId"), SDKConstants.ARCH_PROJECT_ARTIFACT_ID),
-                        element(name("archetypeVersion"), SDKConstants.ARCH_PROJECT_VERSION)
+                        element(name("archetypeVersion"), SDKConstants.ARCH_PROJECT_VERSION),
+                        element(name("basedir"), omrsPath.getPath())
                         //element(name("groupId"), SDKConstants.PROJECT_GROUP_ID),
                         //element(name("artifactId"), serverId),
                         //element(name("version"), version)
                 ),
                 executionEnvironment(mavenProject, mavenSession, pluginManager)
         );
-        // move server after creating
-        File currentPath = new File(System.getProperty("user.dir"), serverId);
-        try {
-            // check if paths are not equal
-            if (!currentPath.equals(serverPath)) FileUtils.moveDirectory(currentPath, serverPath);
-            getLog().info("Server created successfully, path: " + serverPath.getPath());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        // check if any db parameter is set
+        getLog().info("Server created successfully, path: " + serverPath.getPath());
         if ((dbDriver != null) || (dbUser != null) || (dbPassword != null) || (dbUri != null)) {
-            // configure properties
             File propertiesFile = new File(serverPath.getPath(), SDKConstants.OPENMRS_SERVER_PROPERTIES);
             PropertyManager properties = new PropertyManager(propertiesFile.getPath());
             try {
-                // prompt dbDriver if not set
                 String defaultDriver = "mysql";
                 if (dbDriver == null) dbDriver = prompter.prompt("Define value for property 'dbDriver': (default: 'mysql')");
-                // check if default was set
                 if ((dbDriver == null) || (dbDriver.equals(""))) dbDriver = defaultDriver;
-                // get default uri for selected dbDriver
                 String defaultUri = SDKConstants.URI_MYSQL;
-                // postgres shortcut
                 if ((dbDriver.equals("postgresql")) || (dbDriver.equals(SDKConstants.DRIVER_POSTGRESQL))) {
                     properties.setParam("dbDriver", SDKConstants.DRIVER_POSTGRESQL);
-                    // update default uri for postgres
                     defaultUri = SDKConstants.URI_POSTGRESQL;
                 }
-                // h2 shortcut
                 else if ((dbDriver.equals("h2")) || (dbDriver.equals(SDKConstants.DRIVER_H2))) {
                     properties.setParam("dbDriver", SDKConstants.DRIVER_H2);
-                    // update default value for h2
                     defaultUri = SDKConstants.URI_H2;
                 }
-                // mysql shortcut
                 else if (dbDriver.equals("mysql")) properties.setParam("dbDriver", SDKConstants.DRIVER_MYSQL);
-                    // any other drivers
                 else properties.setParam("dbDriver", dbDriver);
-                // set dbUri if not set
+
                 if (dbUri == null) dbUri = prompter.prompt("Define value for property 'dbUri': (default: '" + defaultUri + "')");
-                // check if user choose default uri ('enter' pressed)
                 if ((dbUri == null) || (dbUri.equals(""))) dbUri = defaultUri;
-                // set dbUser property
+
                 String defaultUser = "root";
                 if (dbUser == null) dbUser = prompter.prompt("Define value for property 'dbUser': (default: '" + defaultUser + "')");
-                // check if default was set
                 if ((dbUser == null) || (dbUser.equals(""))) dbUser = defaultUser;
-                // set dbPassword value
                 if (dbPassword == null) dbPassword = prompter.prompt("Define value for property 'dbPassword'");
-                // set other params
+
                 properties.setParam("dbDriver", dbDriver);
                 properties.setParam("dbUser", dbUser);
                 properties.setParam("dbPassword", dbPassword);
@@ -254,7 +220,6 @@ public class SetupPlatform extends AbstractMojo {
     }
 
     public void execute() throws MojoExecutionException {
-        // create and configure server
         setup(serverId, version, dbDriver, dbUri, dbUser, dbPassword, interactiveMode);
     }
 }
