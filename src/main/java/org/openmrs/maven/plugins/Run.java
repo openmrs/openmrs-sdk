b1ad97bdaa5db7dcd73dcd9d267add497b92ab7d
diospark <dmytro.trifonov@gmail.com>
2015-06-21 00:53:33 +0300

    SDK-36 implemented all functions

--- src/main/java/org/openmrs/maven/plugins/Run.java
+++ src/main/java/org/openmrs/maven/plugins/Run.java
@@ -5,9 +5,10 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.codehaus.plexus.components.interactivity.Prompter;
 import org.openmrs.maven.plugins.utility.AttributeHelper;
-import org.openmrs.maven.plugins.utility.OS;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
 
 /**
  * @goal run
@@ -31,18 +32,46 @@ public class Run extends AbstractMojo {
         AttributeHelper helper = new AttributeHelper(prompter);
         ModuleInstall installer = new ModuleInstall(prompter);
         File serverPath = installer.getServerPath(helper, serverId, NO_SERVER_TEXT);
-        OS os = new OS();
-        String task = String.format("cd %s && mvn clean install && cd server && mvn jetty:run && echo", serverPath.getPath());
         try {
-            Process p = os.executeCommand(task);
-            /*
-            StringBuffer output = new StringBuffer();
-            BufferedReader reader =  new BufferedReader(new InputStreamReader(p.getInputStream()));
-            String line = "";
-            while ((line = reader.readLine())!= null) {
-                output.append(line + "\n");
+            ProcessBuilder pb = new ProcessBuilder("mvn", "clean", "install");
+            pb.directory(serverPath);
+            Process p = pb.start();
+            BufferedReader errInput = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            // if there are not errors, run next task
+            String error = null;
+            if ((error = errInput.readLine()) == null) {
+                BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                String s = null;
+                while ((s = stdInput.readLine()) != null) {
+                    System.out.println(s);
+                }
+                File server = new File(serverPath, "server");
+                ProcessBuilder pb2 = new ProcessBuilder("mvn", "jetty:run");
+                pb2.directory(server);
+                Process p2 = pb2.start();
+                BufferedReader errInput2 = new BufferedReader(new InputStreamReader(p2.getErrorStream()));
+                if ((error = errInput2.readLine()) == null) {
+                    BufferedReader stdInput2 = new BufferedReader(new InputStreamReader(p2.getInputStream()));
+                    while ((s = stdInput2.readLine()) != null) {
+                        System.out.println(s);
+                    }
+                }
+                else {
+                    System.out.println(error);
+                    while ((error = errInput2.readLine()) != null) {
+                        System.out.println(error);
+                    }
+                    throw new MojoExecutionException("There are error during starting server");
+                }
             }
-            getLog().info(output.toString()); */
+            else {
+                System.out.println(error);
+                while ((error = errInput.readLine()) != null) {
+                    System.out.println(error);
+                }
+                throw new MojoExecutionException("There are error during installing server");
+            }
+
         } catch (Exception e) {
             getLog().error(e.getMessage());
         }
