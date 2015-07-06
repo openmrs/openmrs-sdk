e713ed1e10ba83affbce2de58ab53fe467d01c58
diospark <dmytro.trifonov@gmail.com>
2015-07-07 00:30:24 +0300

    SDK-47 Fixed error with version types

--- src/main/java/org/openmrs/maven/plugins/model/Version.java
+++ src/main/java/org/openmrs/maven/plugins/model/Version.java
@@ -1,39 +1,15 @@
 package org.openmrs.maven.plugins.model;
 
-public class Version implements Comparable<Version> {
+import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 
-    private String version;
-
-    public final String get() {
-        return this.version;
-    }
+public class Version extends DefaultArtifactVersion {
 
     public Version(String version) {
-        if (version == null)
-            throw new IllegalArgumentException("Version can not be null");
-        if (!version.matches("[0-9]+(\\.[0-9]+)*"))
-            throw new IllegalArgumentException("Invalid version format");
-        this.version = version;
+        super(version);
     }
 
-    @Override
-    public int compareTo(Version that) {
-        if (that == null)
-            return 1;
-        String[] thisParts = this.get().split("\\.");
-        String[] thatParts = that.get().split("\\.");
-        int length = Math.max(thisParts.length, thatParts.length);
-        for (int i = 0; i < length; i++) {
-            int thisPart = i < thisParts.length ?
-                    Integer.parseInt(thisParts[i]) : 0;
-            int thatPart = i < thatParts.length ?
-                    Integer.parseInt(thatParts[i]) : 0;
-            if (thisPart < thatPart)
-                return -1;
-            if (thisPart > thatPart)
-                return 1;
-        }
-        return 0;
+    public boolean lower(Version that) {
+        return this.compareTo(that) == -1;
     }
 
     public boolean equal(Version that) {
@@ -42,14 +18,5 @@ public class Version implements Comparable<Version> {
 
     public boolean higher(Version that) {
         return this.compareTo(that) == 1;
-    }
-
-    public boolean lower(Version that) {
-        return this.compareTo(that) == -1;
-    }
-
-    @Override
-    public String toString() {
-        return version != null ? version : "null";
     }
 }
