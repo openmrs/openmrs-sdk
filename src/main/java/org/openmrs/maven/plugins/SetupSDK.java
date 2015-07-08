9674db7056efeba4f9b2a4f43cec5f3b1904a60a
diospark <dmytro.trifonov@gmail.com>
2015-07-08 11:21:11 +0300

    SDK-51 removed "maven.remote.repo" param

--- src/main/java/org/openmrs/maven/plugins/SetupSDK.java
+++ src/main/java/org/openmrs/maven/plugins/SetupSDK.java
@@ -19,13 +19,6 @@ public class SetupSDK extends AbstractMojo{
     private static final String SUCCESS_TEMPLATE = "SDK installed successfully, settings file: %s";
     private static final String SDK_INFO = "Now you can use sdk: mvn openmrs-sdk:<task_name>";
 
-    /**
-     * OpenMRS Maven Repo
-     *
-     * @parameter expression="${maven.remote.repo}" default-value="http://mavenrepo.openmrs.org/nexus/content/repositories/public"
-     */
-    private String repo;
-
     public void execute() throws MojoExecutionException, MojoFailureException {
         File mavenHome = new File(System.getProperty("user.home"), SDKConstants.MAVEN_SETTINGS_FOLDER);
         mavenHome.mkdirs();
@@ -33,8 +26,9 @@ public class SetupSDK extends AbstractMojo{
         try {
             SettingsManager settings = null;
             if (!mavenSettings.exists()) {
-                mavenSettings.createNewFile();
                 settings = new SettingsManager();
+                OutputStream emptySettings = new FileOutputStream(mavenSettings);
+                settings.apply(emptySettings);
             }
             else {
                 InputStream stream = new FileInputStream(mavenSettings);
@@ -42,9 +36,7 @@ public class SetupSDK extends AbstractMojo{
             }
             InputStream settingsStream = getClass().getClassLoader().getResourceAsStream(SDKConstants.MAVEN_SETTINGS);
             SettingsManager defaultSettings = new SettingsManager(settingsStream);
-            // truncate if url ends with '/'
-            String validUrl = repo.endsWith("/") ? repo.substring(0, repo.lastIndexOf("/")) : repo;
-            settings.updateSettings(defaultSettings.getSettings(), validUrl);
+            settings.updateSettings(defaultSettings.getSettings());
             OutputStream out = new FileOutputStream(mavenSettings);
             settings.apply(out);
             getLog().info(String.format(SUCCESS_TEMPLATE, mavenSettings.getPath()));
