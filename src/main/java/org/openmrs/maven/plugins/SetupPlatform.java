ade540e1a7ed842ca3ca446f1ea4dbd928e4547b
Dmytro Trifonov <dmytro.trifonov@gmail.com>
2015-08-10 00:00:58 +0300

    SDK-61 clean unused

--- src/main/java/org/openmrs/maven/plugins/SetupPlatform.java
+++ src/main/java/org/openmrs/maven/plugins/SetupPlatform.java
@@ -21,9 +21,7 @@ import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static org.twdata.maven.mojoexecutor.MojoExecutor.*;
 
