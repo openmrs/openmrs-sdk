ce870e32113c5482bf02c1843f58d1cb65553181
Tomasz Marzeion <marzeion.tomasz@gmail.com>
2016-07-06 11:24:07 +0200

    Minor cleanup (cherry picked from commit e8b64221c5910b7375fe461cfb7e77d80a9b8300)

--- maven-plugin/src/main/java/org/openmrs/maven/plugins/utility/DefaultWizard.java
+++ maven-plugin/src/main/java/org/openmrs/maven/plugins/utility/DefaultWizard.java
@@ -353,7 +353,7 @@ public class DefaultWizard implements Wizard {
             try {
                 sdkPropertiesFile.createNewFile();
             } catch (IOException e) {
-                throw new IllegalStateException("Failed to create SDK properties file in: \"" + Server.getServersPathFile() + SDKConstants.OPENMRS_SDK_PROPERTIES + "\"");
+                throw new IllegalStateException("Failed to create SDK properties file in: \"" + Server.getServersPathFile() + "/" + SDKConstants.OPENMRS_SDK_PROPERTIES + "\"");
             }
         }
 
@@ -361,7 +361,7 @@ public class DefaultWizard implements Wizard {
         try {
             in = new FileInputStream(sdkPropertiesFile);
         } catch (FileNotFoundException e) {
-            throw new IllegalStateException("SDK properties file not found at: \"" + Server.getServersPathFile() + SDKConstants.OPENMRS_SDK_PROPERTIES + "\"");
+            throw new IllegalStateException("SDK properties file not found at: \"" + Server.getServersPathFile() + "/" + SDKConstants.OPENMRS_SDK_PROPERTIES + "\"");
         }
         Properties sdkProperties = new Properties();
         try {
@@ -377,7 +377,6 @@ public class DefaultWizard implements Wizard {
         List<String> result = new ArrayList<>();
 
         if (!interactiveMode) {
-            addJdkPathToSdkProperties("Some/Mock/Path/");
             return new ArrayList<>();
         }
 
