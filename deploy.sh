870c9a297e4eb61a15bce9e860d6a6242309ba53
Rafal Korytkowski <rafal@openmrs.org>
2016-08-04 15:38:41 +0200

    Fixing release script

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,10 @@
 #     - GITHUB_API_KEY - the one generated above
 # 3) Go to Bintray and create new openmrs-sdk package in maven repo
 
-
+LAST_COMMIT = `git log --oneline -1 | grep "Releasing $TRAVIS_TAG"`
+if [ -z "$LAST_COMMIT" ]
+then 
+echo "Setting version to $TRAVIS_TAG"
 mvn versions:set -DnewVersion=$TRAVIS_TAG
 git tag -d $TRAVIS_TAG
 git push --quiet "https://$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG.git" :refs/tags/$TRAVIS_TAG > /dev/null 2>&1
@@ -16,4 +19,7 @@ git add -u
 git commit -m "Releasing $TRAVIS_TAG"
 git tag $TRAVIS_TAG
 git push --quiet "https://$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG.git" $TRAVIS_TAG > /dev/null 2>&1
+else
+echo "Releasing version $TRAVIS_TAG"
 mvn deploy -Pbintray
+fi
