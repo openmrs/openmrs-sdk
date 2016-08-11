7c39dd85f019d24e211ea374834272af540b2304
AdamGrzybkowski <agrzybkowski@outlook.com>
2016-08-11 08:36:34 +0200

    SDK-85 add create OWA to create-project goal

--- integration-tests/src/test/java/org/openmrs/maven/plugins/CreateProjectIntegrationTest.java
+++ integration-tests/src/test/java/org/openmrs/maven/plugins/CreateProjectIntegrationTest.java
@@ -6,8 +6,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
-import java.util.Arrays;
-import java.util.List;
 
 public class CreateProjectIntegrationTest extends AbstractSdkIntegrationTest {
 
