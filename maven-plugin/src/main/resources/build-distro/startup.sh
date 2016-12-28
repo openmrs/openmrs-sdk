98d33ef69ae4dac90aa45fa2533479e33666bb28
tmarzeion <marzeion.tomasz@gmail.com>
2016-12-28 10:17:47 +0100

    SDK-181 - Increased docker MySQL initialization time

--- maven-plugin/src/main/resources/build-distro/startup.sh
+++ maven-plugin/src/main/resources/build-distro/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # wait for mysql to initialise
-/usr/local/tomcat/wait-for-it.sh --timeout=120 mysql:3306
+/usr/local/tomcat/wait-for-it.sh --timeout=3600 mysql:3306
 
 # start tomcat
 /usr/local/tomcat/bin/catalina.sh run
