521c6c3e5af66d178a3fb687f7144399ba2eb141
Ian <52504170+ibacher@users.noreply.github.com>
2021-04-30 13:48:25 -0400

    SDK-275: Support module names with dashes (#180)

--- maven-plugin/src/test/java/org/openmrs/maven/plugins/model/ArtifactTest.java
+++ maven-plugin/src/test/java/org/openmrs/maven/plugins/model/ArtifactTest.java
@@ -8,7 +8,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ArtifactTest {
 
     @Test
-    public void testGetDestFileName() throws Exception{
+    public void testGetDestFileName() {
         Artifact nodashes = new Artifact("modulename-omod","1.4-SNAPSHOT");
         assertThat(nodashes.getDestFileName(), is("modulename-1.4-SNAPSHOT.jar"));
 
@@ -17,5 +17,8 @@ public class ArtifactTest {
 
         Artifact withdoubledashes = new Artifact("modulename-submodule-subsubmodule-omod","5.7.1");
         assertThat(withdoubledashes.getDestFileName(), is("modulename-submodule-subsubmodule-5.7.1.jar"));
+
+        Artifact withoutomod = new Artifact("modulename-anothername", "1.0.0");
+        assertThat(withoutomod.getDestFileName(), is("modulename-anothername-1.0.0.jar"));
     }
 }
