bd69cf16bd62eb0bc429a6f4bb6963a5e21da461
mherman22 <hermanmuhereza22@gmail.com>
2024-08-12 22:35:55 +0300

    SDK to rebuild frontend with multiple config files

--- maven-plugin/src/main/java/org/openmrs/maven/plugins/utility/SpaInstaller.java
+++ maven-plugin/src/main/java/org/openmrs/maven/plugins/utility/SpaInstaller.java
@@ -12,9 +12,11 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
@@ -22,31 +24,31 @@ import java.util.Set;
 import static org.openmrs.maven.plugins.utility.PropertiesUtils.getSdkProperties;
 
 public class SpaInstaller {
-	
+
 	static final String BAD_SPA_PROPERTIES_MESSAGE = "Distro properties file contains invalid 'spa.' elements. " +
 			"Please check the distro properties file and the specification. " +
 			"Parent properties cannot have their own values (i.e., if 'spa.foo.bar' exists, 'spa.foo' cannot be assigned a value). "
 			+
 			"Duplicate properties are not allowed.";
-	
+
 	static final String NODE_VERSION = "16.15.0";
-	
+
 	static final String NPM_VERSION = "8.5.5";
-	
+
 	static final String BUILD_TARGET_DIR = "frontend";
-	
+
 	private final NodeHelper nodeHelper;
-	
+
 	private final DistroHelper distroHelper;
 
 	private static final Logger logger = LoggerFactory.getLogger(SpaInstaller.class);
-	
+
 	public SpaInstaller(DistroHelper distroHelper,
-			NodeHelper nodeHelper) {
+						NodeHelper nodeHelper) {
 		this.distroHelper = distroHelper;
 		this.nodeHelper = nodeHelper;
 	}
-	
+
 	/**
 	 * Installs the SPA Microfrontend application based on entries in the distro properties.
 	 *
@@ -88,6 +90,10 @@ public class SpaInstaller {
 			File spaConfigFile = new File(appDataDir, "spa-build-config.json");
 			writeJSONObject(spaConfigFile, spaConfigJson);
 
+			List<String> configFilePaths = new ArrayList<>();
+			configFilePaths.add(spaConfigFile.getAbsolutePath());
+			// You can add more config files to the list if needed
+
 			Properties sdkProperties = getSdkProperties();
 			boolean reuseNodeCache = (overrideReuseNodeCache != null) ? overrideReuseNodeCache : Boolean.parseBoolean(sdkProperties.getProperty("reuseNodeCache"));
 			nodeHelper.installNodeAndNpm(nodeVersion, npmVersion, reuseNodeCache);
@@ -97,10 +103,16 @@ public class SpaInstaller {
 			String legacyPeerDeps = ignorePeerDependencies ? "--legacy-peer-deps" : "";
 			// print frontend tool version number
 			nodeHelper.runNpx(String.format("%s --version", program), legacyPeerDeps);
+
+			// Execute the build command with the generated config file
 			nodeHelper.runNpx(
 					String.format("%s build --target %s --build-config %s", program, buildTargetDir, spaConfigFile), legacyPeerDeps);
+
+			String allConfigFilePaths = String.join(" ", configFilePaths);
+
+			// Execute the assemble command with all config files
 			nodeHelper.runNpx(
-					String.format("%s assemble --target %s --mode config --config %s", program, buildTargetDir, spaConfigFile), legacyPeerDeps);
+					String.format("%s assemble --target %s --mode config --config %s", program, buildTargetDir, allConfigFilePaths), legacyPeerDeps);
 
 			Path nodeCache = NodeHelper.tempDir;
 			if (!reuseNodeCache) {
@@ -114,7 +126,7 @@ public class SpaInstaller {
 			}
 		}
 	}
-	
+
 	private Map<String, Object> convertPropertiesToJSON(Map<String, String> properties) throws MojoExecutionException {
 		Set<String> foundPropertySetKeys = new HashSet<>();
 		Map<String, Object> result = new LinkedHashMap<>();
@@ -124,7 +136,7 @@ public class SpaInstaller {
 				foundPropertySetKeys.add(StringUtils.join(Arrays.copyOfRange(keys, 0, i), "."));
 			}
 		}
-		
+
 		for (String dotDelimitedKeys : properties.keySet()) {
 			if (foundPropertySetKeys.contains(dotDelimitedKeys)) {
 				String badLine = "spa." + dotDelimitedKeys + "=" + properties.get(dotDelimitedKeys);
@@ -134,10 +146,10 @@ public class SpaInstaller {
 			}
 			addPropertyToJSONObject(result, dotDelimitedKeys, properties.get(dotDelimitedKeys));
 		}
-		
+
 		return result;
 	}
-	
+
 	/**
 	 * Add a line from the properties file to the new JSON object. Creates nested objects as needed.
 	 * Known array-valued keys are parsed as comma-delimited arrays; e.g.,
@@ -150,14 +162,14 @@ public class SpaInstaller {
 	private void addPropertyToJSONObject(Map<String, Object> jsonObject, String propertyKey, String value)
 			throws MojoExecutionException {
 		String[] keys = propertyKey.split("\\.");
-		
+
 		if (keys.length == 1) {
 			if (jsonObject.containsKey(keys[0])) {
 				throw new MojoExecutionException(BAD_SPA_PROPERTIES_MESSAGE +
 						" Encountered this error processing a property containing the key '" + keys[0] + "' and with value "
 						+ value);
 			}
-			
+
 			if ("configUrls".equals(keys[0])) {
 				String[] urls = value.split(",");
 				jsonObject.put(keys[0], urls);
@@ -168,20 +180,20 @@ public class SpaInstaller {
 			if (!jsonObject.containsKey(keys[0])) {
 				jsonObject.put(keys[0], new LinkedHashMap<String, Object>());
 			}
-			
+
 			Object childObject = jsonObject.get(keys[0]);
 			if (!(childObject instanceof Map)) {
 				throw new MojoExecutionException(BAD_SPA_PROPERTIES_MESSAGE +
 						" Also please post to OpenMRS Talk and include this full message. If you are seeing this, there has been a programming error.");
 			}
-			
+
 			@SuppressWarnings("unchecked")
 			Map<String, Object> child = (Map<String, Object>) childObject;
 			String childKeys = StringUtils.join(Arrays.copyOfRange(keys, 1, keys.length), ".");
 			addPropertyToJSONObject(child, childKeys, value);
 		}
 	}
-	
+
 	private static void writeJSONObject(File file, Map<String, Object> jsonObject) throws MojoExecutionException {
 		ObjectMapper om = new ObjectMapper();
 		try {
@@ -192,5 +204,5 @@ public class SpaInstaller {
 					+ e.getMessage(), e);
 		}
 	}
-	
+
 }
--- maven-plugin/src/test/java/org/openmrs/maven/plugins/utility/SpaInstallerTest.java
+++ maven-plugin/src/test/java/org/openmrs/maven/plugins/utility/SpaInstallerTest.java
@@ -10,12 +10,15 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.openmrs.maven.plugins.model.DistroProperties;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -61,5 +64,27 @@ public class SpaInstallerTest {
         File spaConfigFile = new File(appDataDir, "spa-build-config.json");
         String spaConfig = FileUtils.readFileToString(spaConfigFile, StandardCharsets.UTF_8);
         Assert.assertThat(spaConfig, Matchers.containsString("\"configUrls\":[\"foo\",\"bar\",\"baz\"]"));
+    }
+
+    @Test
+    public void spaInstall_shouldCreateBuildAndAssembleCommandWithSingleConfigFile() throws MojoExecutionException {
+        Properties distroProperties = new Properties();
+        distroProperties.setProperty("spa.configUrls", "foo");
+        distroProperties.setProperty("spa.configUrls2", "bar");
+        distroProperties.setProperty("spa.configUrls3", "baz");
+
+        spaInstaller.installFromDistroProperties(appDataDir, new DistroProperties(distroProperties));
+
+        Mockito.verify(nodeHelper).installNodeAndNpm("16.15.0", "8.5.5", false);
+        Mockito.verify(nodeHelper).runNpx("openmrs@next --version", "");
+        Mockito.verify(nodeHelper).runNpx(
+                "openmrs@next build --target " + appDataDir.getAbsolutePath() + "/frontend --build-config " + appDataDir.getAbsolutePath() + "/spa-build-config.json" ,
+                ""
+        );
+
+        Mockito.verify(nodeHelper).runNpx(
+                "openmrs@next assemble --target " + appDataDir.getAbsolutePath() + "/frontend --mode config --config " + appDataDir.getAbsolutePath() + "/spa-build-config.json" ,
+                ""
+        );
     }
 }
