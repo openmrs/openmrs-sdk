605240440f5212d03b873f9aee72fe9dbf9b4ced
nravilla <nirupakanthi@gmail.com>
2024-08-27 07:33:54 -0400

    Formatting fix

--- maven-plugin/src/main/java/org/openmrs/maven/plugins/AbstractTask.java
+++ maven-plugin/src/main/java/org/openmrs/maven/plugins/AbstractTask.java
@@ -34,138 +34,138 @@ import java.util.ArrayDeque;
  * Base class for all OpenMRS SDK Maven Mojos
  */
 public abstract class AbstractTask extends AbstractMojo {
-
+	
 	/**
 	 * The project currently being build
 	 */
 	@Parameter(defaultValue = "${project}", readonly = true)
 	MavenProject mavenProject;
-
+	
 	/**
 	 * The current Maven session.
 	 */
 	@Parameter(defaultValue = "${session}", readonly = true, required = true)
 	MavenSession mavenSession;
-
+	
 	/**
 	 * The Maven Settings
 	 */
 	@Parameter(defaultValue = "${settings}", readonly = true)
 	Settings settings;
-
+	
 	/**
 	 * test mode, if true disables interactive mode and uses batchAnswers, even if there is none
 	 */
 	@Parameter(defaultValue = "false", property = "testMode")
 	boolean testMode;
-
+	
 	/**
 	 * path to openmrs directory
 	 */
 	@Parameter(property = "openMRSPath")
 	String openMRSPath;
-
+	
 	/***
 	 * answers to use if not running in interactive mode
 	 */
 	@Parameter(property = "batchAnswers")
 	ArrayDeque<String> batchAnswers;
-
+	
 	/**
 	 * stats
 	 */
 	@Parameter(defaultValue = "false", property = "stats")
 	boolean stats;
-
-        /**
-     * The artifact metadata source to use.
-     */
-    @Component
-    ArtifactMetadataSource artifactMetadataSource;
-
-    @Component
-    ArtifactFactory artifactFactory;
-
-    /**
-     * The Maven BuildPluginManager component.
-     */
-    @Component
-    BuildPluginManager pluginManager;
-
-    @Component
-    Wizard wizard;
-
+	
+	/**
+	 * The artifact metadata source to use.
+	 */
+	@Component
+	ArtifactMetadataSource artifactMetadataSource;
+	
+	@Component
+	ArtifactFactory artifactFactory;
+	
+	/**
+	 * The Maven BuildPluginManager component.
+	 */
+	@Component
+	BuildPluginManager pluginManager;
+	
+	@Component
+	Wizard wizard;
+	
 	/**
 	 * wizard for resolving artifact available versions
 	 */
 	VersionsHelper versionsHelper;
-
+	
 	/**
 	 * handles installing modules on server
 	 */
 	ModuleInstaller moduleInstaller;
-
+	
 	/**
 	 * handles distro-properties
 	 */
 	DistroHelper distroHelper;
-
+	
 	/**
 	 * handles OWAs
 	 */
 	OwaHelper owaHelper;
 	
-    /**
-     * handles Contents
-     */
-    ContentHelper contentHelper;
-
+	/**
+	 * handles Contents
+	 */
+	ContentHelper contentHelper;
+	
 	/**
 	 * installs SPAs
 	 */
 	SpaInstaller spaInstaller;
-
+	
 	/**
 	 * handles github and provides basic git utilities
 	 */
 	GitHelper gitHelper;
-
+	
 	/**
 	 * handles jira
 	 */
 	Jira jira;
-
+	
 	/**
 	 * handles docker
 	 */
 	DockerHelper dockerHelper;
-
+	
 	public AbstractTask() {
 	}
-
-    public AbstractTask(AbstractTask other) {
-        this.mavenProject = other.mavenProject;
-        this.mavenSession = other.mavenSession;
-        this.wizard = other.wizard;
-        this.pluginManager = other.pluginManager;
-        this.artifactFactory = other.artifactFactory;
-        this.artifactMetadataSource = other.artifactMetadataSource;
-        this.moduleInstaller = other.moduleInstaller;
-        this.versionsHelper = other.versionsHelper;
-        this.distroHelper = other.distroHelper;
-        this.owaHelper = other.owaHelper;
-        this.contentHelper = other.contentHelper;
-        this.spaInstaller = other.spaInstaller;
-        this.gitHelper = other.gitHelper;
-        this.dockerHelper = other.dockerHelper;
-        this.settings = other.settings;
-        this.batchAnswers = other.batchAnswers;
-        this.testMode = other.testMode;
-        this.openMRSPath = other.openMRSPath;
-        this.stats = other.stats;
-        initTask();
+	
+	public AbstractTask(AbstractTask other) {
+		this.mavenProject = other.mavenProject;
+		this.mavenSession = other.mavenSession;
+		this.wizard = other.wizard;
+		this.pluginManager = other.pluginManager;
+		this.artifactFactory = other.artifactFactory;
+		this.artifactMetadataSource = other.artifactMetadataSource;
+		this.moduleInstaller = other.moduleInstaller;
+		this.versionsHelper = other.versionsHelper;
+		this.distroHelper = other.distroHelper;
+		this.owaHelper = other.owaHelper;
+		this.contentHelper = other.contentHelper;
+		this.spaInstaller = other.spaInstaller;
+		this.gitHelper = other.gitHelper;
+		this.dockerHelper = other.dockerHelper;
+		this.settings = other.settings;
+		this.batchAnswers = other.batchAnswers;
+		this.testMode = other.testMode;
+		this.openMRSPath = other.openMRSPath;
+		this.stats = other.stats;
+		initTask();
 	}
-
+	
 	public void initTask() {
 		if (jira == null) {
 			jira = new DefaultJira();
@@ -182,34 +182,34 @@ public abstract class AbstractTask extends AbstractMojo {
 		if (distroHelper == null) {
 			distroHelper = new DistroHelper(mavenProject, mavenSession, pluginManager, wizard, versionsHelper);
 		}
-        if (owaHelper == null) {
-            owaHelper = new OwaHelper(mavenSession, mavenProject, pluginManager, wizard);
-        }
-        if (contentHelper == null) {
-            contentHelper = new ContentHelper(mavenSession, mavenProject, pluginManager);
-        }
-        if (spaInstaller == null) {
-            spaInstaller = new SpaInstaller(distroHelper, new NodeHelper(mavenProject, mavenSession, pluginManager));
-        }
+		if (owaHelper == null) {
+			owaHelper = new OwaHelper(mavenSession, mavenProject, pluginManager, wizard);
+		}
+		if (contentHelper == null) {
+			contentHelper = new ContentHelper(mavenSession, mavenProject, pluginManager);
+		}
+		if (spaInstaller == null) {
+			spaInstaller = new SpaInstaller(distroHelper, new NodeHelper(mavenProject, mavenSession, pluginManager));
+		}
 		if (dockerHelper == null) {
 			dockerHelper = new DockerHelper(mavenProject, mavenSession, pluginManager, wizard);
 		}
 		if (StringUtils.isNotBlank(openMRSPath)) {
 			Server.setServersPath(openMRSPath);
 		}
-
+		
 		if ((batchAnswers != null && !batchAnswers.isEmpty()) || testMode) {
 			wizard.setAnswers(batchAnswers);
 			wizard.setInteractiveMode(false);
 		}
 	}
-
+	
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
 		initTask();
 		new StatsManager(wizard, mavenSession, stats).incrementGoalStats();
 		executeTask();
 	}
-
-    abstract public void executeTask() throws MojoExecutionException, MojoFailureException;
+	
+	abstract public void executeTask() throws MojoExecutionException, MojoFailureException;
 }
