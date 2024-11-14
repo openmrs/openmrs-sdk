4205d397441a4b8e95a918835f2ddee94287401c
Michael Seaton <mseaton@pih.org>
2024-11-14 16:11:28 -0500

    Fix merge issues

--- integration-tests/src/test/java/org/openmrs/maven/plugins/AddExclusionIntegrationTest.java
+++ integration-tests/src/test/java/org/openmrs/maven/plugins/AddExclusionIntegrationTest.java
@@ -1,8 +1,11 @@
 package org.openmrs.maven.plugins;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.openmrs.maven.plugins.model.DistroProperties;
 import org.openmrs.maven.plugins.utility.DistroHelper;
+
+import java.io.File;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
@@ -11,78 +14,65 @@ import static org.junit.Assert.assertTrue;
 
 public class AddExclusionIntegrationTest extends AbstractSdkIntegrationTest {
 
+    public DistroProperties getDistroProperties() {
+        return DistroHelper.getDistroPropertiesFromFile(distroFile);
+    }
+
     @Test
     public void shouldAddExclusionIfNoParentDefined() throws Exception {
-        useDistroProperties("openmrs-distro.properties");
-        DistroProperties distroProperties = DistroHelper.getDistroPropertiesFromFile(distroFile);
-        assertNotNull(distroProperties);
-        assertNull(distroProperties.getParentDistroArtifact());
-        assertTrue(distroProperties.getExclusions().isEmpty());
+        includeDistroPropertiesFile("openmrs-distro.properties");
+        assertNotNull(getDistroProperties());
+        assertNull(getDistroProperties().getParentDistroArtifact());
+        assertTrue(getDistroProperties().getExclusions().isEmpty());
         executeExclusionTask(distroFile, "omod.uicommons");
-        distroProperties = DistroHelper.getDistroPropertiesFromFile(distroFile);
-        assertNotNull(distroProperties);
-        assertTrue(distroProperties.getExclusions().contains("omod.uicommons"));
+        assertNotNull(getDistroProperties());
+        assertTrue(getDistroProperties().getExclusions().contains("omod.uicommons"));
         assertTrue(getLogContents().contains(AddExclusion.WARNING_NO_PARENT_DISTRO));
     }
 
     @Test
     public void shouldAddExclusionForPropertyContainedInParent() throws Exception {
-        useDistroProperties("openmrs-distro-parent-as-parent.properties");
-        DistroProperties distroProperties = DistroHelper.getDistroPropertiesFromFile(distroFile);
-        assertNotNull(distroProperties);
-        assertNotNull(distroProperties.getParentDistroArtifact());
-        assertTrue(distroProperties.getExclusions().isEmpty());
+        includeDistroPropertiesFile("openmrs-distro-parent-as-parent.properties");
+        assertNotNull(getDistroProperties());
+        assertNotNull(getDistroProperties().getParentDistroArtifact());
+        assertTrue(getDistroProperties().getExclusions().isEmpty());
         executeExclusionTask(distroFile, "omod.uicommons");
-        distroProperties = DistroHelper.getDistroPropertiesFromFile(distroFile);
-        assertNotNull(distroProperties);
-        assertTrue(distroProperties.getExclusions().contains("omod.uicommons"));
+        assertNotNull(getDistroProperties());
+        assertTrue(getDistroProperties().getExclusions().contains("omod.uicommons"));
         assertFalse(getLogContents().contains(AddExclusion.WARNING_NO_PARENT_DISTRO));
         assertFalse(getLogContents().contains(AddExclusion.WARNING_PROPERTY_NOT_IN_PARENT));
     }
 
     @Test
     public void shouldAddExclusionForPropertyContainedInDistro() throws Exception {
-        useDistroProperties("openmrs-distro-parent-as-distro.properties");
-        DistroProperties distroProperties = DistroHelper.getDistroPropertiesFromFile(distroFile);
-        assertNotNull(distroProperties);
-        assertNotNull(distroProperties.getParentDistroArtifact());
-        assertTrue(distroProperties.getExclusions().isEmpty());
+        includeDistroPropertiesFile("openmrs-distro-parent-as-distro.properties");
+        assertNotNull(getDistroProperties());
+        assertNotNull(getDistroProperties().getParentDistroArtifact());
+        assertTrue(getDistroProperties().getExclusions().isEmpty());
         executeExclusionTask(distroFile, "omod.uicommons");
-        distroProperties = DistroHelper.getDistroPropertiesFromFile(distroFile);
-        assertNotNull(distroProperties);
-        assertTrue(distroProperties.getExclusions().contains("omod.uicommons"));
+        assertNotNull(getDistroProperties());
+        assertTrue(getDistroProperties().getExclusions().contains("omod.uicommons"));
         assertFalse(getLogContents().contains(AddExclusion.WARNING_NO_PARENT_DISTRO));
         assertFalse(getLogContents().contains(AddExclusion.WARNING_PROPERTY_NOT_IN_PARENT));
     }
 
     @Test
     public void shouldAddExclusionForPropertyNotContainedInParent() throws Exception {
-        useDistroProperties("openmrs-distro-parent-as-parent.properties");
-        DistroProperties distroProperties = DistroHelper.getDistroPropertiesFromFile(distroFile);
-        assertNotNull(distroProperties);
-        assertNotNull(distroProperties.getParentDistroArtifact());
-        assertTrue(distroProperties.getExclusions().isEmpty());
+        includeDistroPropertiesFile("openmrs-distro-parent-as-parent.properties");
+        assertNotNull(getDistroProperties());
+        assertNotNull(getDistroProperties().getParentDistroArtifact());
+        assertTrue(getDistroProperties().getExclusions().isEmpty());
         executeExclusionTask(distroFile, "omod.invalidmodulename");
-        distroProperties = DistroHelper.getDistroPropertiesFromFile(distroFile);
-        assertNotNull(distroProperties);
-        assertTrue(distroProperties.getExclusions().contains("omod.invalidmodulename"));
+        assertNotNull(getDistroProperties());
+        assertTrue(getDistroProperties().getExclusions().contains("omod.invalidmodulename"));
         assertFalse(getLogContents().contains(AddExclusion.WARNING_NO_PARENT_DISTRO));
         assertTrue(getLogContents().contains(AddExclusion.WARNING_PROPERTY_NOT_IN_PARENT));
     }
-
-
 
     private String getLogContents() throws Exception {
         File logFile = new File(testDirectory, "log.txt");
         assertTrue(logFile.exists());
         return FileUtils.readFileToString(logFile, "UTF-8");
-    }
-
-    private void useDistroProperties(String fileName) throws Exception {
-        File testFile = new File(testDirectory, fileName);
-        if (!testFile.equals(distroFile)) {
-            FileUtils.copyFile(testFile, distroFile);
-        }
     }
 
     private void executeExclusionTask(File distroFile, String exclusion) throws Exception {
@@ -94,7 +84,5 @@ public class AddExclusionIntegrationTest extends AbstractSdkIntegrationTest {
         }
         executeTask("exclusion");
         assertSuccess();
-        distroProperties = DistroHelper.getDistroPropertiesFromFile(new File(distroFile));
-        assertTrue(distroProperties.getExclusions().contains("omod.uicommons"));
     }
 }
