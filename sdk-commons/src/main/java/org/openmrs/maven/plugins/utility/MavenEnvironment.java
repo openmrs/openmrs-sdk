76a7b7724bd505f9018d8cb2e551ce08cef75162
Michael Seaton <mseaton@pih.org>
2024-11-18 23:32:36 -0500

    SDK-358 - Add integration test framework for helper classes

---
+++ sdk-commons/src/main/java/org/openmrs/maven/plugins/utility/MavenEnvironment.java
@@ -0,0 +1,23 @@
+package org.openmrs.maven.plugins.utility;
+
+import lombok.Data;
+import org.apache.maven.artifact.factory.ArtifactFactory;
+import org.apache.maven.artifact.metadata.ArtifactMetadataSource;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.BuildPluginManager;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.settings.Settings;
+
+/**
+ * Component that allows access to the Maven components set within the current execution environment
+ */
+@Data
+public class MavenEnvironment {
+	private MavenProject mavenProject;
+	private MavenSession mavenSession;
+	private Settings settings;
+	private ArtifactMetadataSource artifactMetadataSource;
+	private ArtifactFactory artifactFactory;
+	private BuildPluginManager pluginManager;
+	private Wizard wizard;
+}
