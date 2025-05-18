90ece2da39343a9b75ebb7dbad5754b37290f3ff
mherman22 <hermanmuhereza22@gmail.com>
2025-05-18 09:11:08 +0300

    remove unintended change

--- sdk-commons/src/main/java/org/openmrs/maven/plugins/model/DistroProperties.java
+++ sdk-commons/src/main/java/org/openmrs/maven/plugins/model/DistroProperties.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 
