4cb679ddf6fb90835ae9996ad23c8256dda4785f
mherman22 <hermanmuhereza22@gmail.com>
2025-05-30 08:22:03 +0300

    SDK-385: Improve the Reference Application Module to work out of the box

--- archetype-module-refapp/src/main/resources/archetype-resources/api/src/test/java/dao/__moduleClassnamePrefix__DaoTest.java
+++ archetype-module-refapp/src/main/resources/archetype-resources/api/src/test/java/dao/__moduleClassnamePrefix__DaoTest.java
@@ -10,13 +10,14 @@
  * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
  * graphic logo is a trademark of OpenMRS Inc.
  */
-package ${package}.api.dao;
+package ${package}.dao;
 
 import org.junit.Test;
 import org.junit.Ignore;
 import org.openmrs.api.UserService;
 import org.openmrs.api.context.Context;
 import ${package}.Item;
+import ${package}.api.dao.${moduleClassnamePrefix}Dao;
 import org.openmrs.test.BaseModuleContextSensitiveTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import static org.hamcrest.Matchers.*;
